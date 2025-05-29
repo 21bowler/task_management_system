ba8cf58f3ba0824d6dfbac716a4295dfc28e57d0
21bowler <samwachira224@gmail.com>
2025-05-29 11:24:30 +0300

    getting a single task implemented

--- app/api/tasks/[taskId]/route.ts
+++ app/api/tasks/[taskId]/route.ts
@@ -1,16 +1,43 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import Task from '@/models/task.model';
 
 // Gets a single task by its ID
 export async function GET(
     request: NextRequest,
     { params }: { params: Promise<{ taskId: string }> },
 ) {
-    const {taskId} = await params;
-    // e.g. Query a database for user with ID `id`
-    return new Response(JSON.stringify({ TaskId: taskId, name: `User ${taskId}` }), {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-    });
+    try {
+        const {taskId} = await params;
+
+        const task = await Task.findOne({ _id: taskId })
+        if (!task) {
+            return NextResponse.json({
+                success: false,
+                message: "Task not found"
+            }, {
+                status: 404
+            })
+        }
+        // e.g., Query a database for user with ID `id`
+        return NextResponse.json({
+            success: true,
+            message: "Task successfully fetched",
+            data: {
+                title: task.title,
+                description: task.description,
+                status: task.status,
+                priority: task.priority,
+                dueDate: task.dueDate,
+                tags: task.tags
+            }
+        });
+    } catch (e) {
+        console.error("Error fetching tasks: ", e)
+        NextResponse.json({
+            success: false,
+            error: "Internal Server Error"
+        }, {status: 500})
+    }
 }
 
 export async function DELETE(
