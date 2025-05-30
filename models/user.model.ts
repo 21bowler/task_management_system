36ce79b94c7e5d72ec77f6aeebc4184c9cdeaf01
21bowler <samwachira224@gmail.com>
2025-05-30 21:24:11 +0300

    add user model

---
+++ models/user.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    trim: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters"],
+    select: false,
+  }
+}, { timestamps: true})
+
+const User = mongoose.models.User || mongoose.model<IUser>('User', userSchema);
+
+export default User;
