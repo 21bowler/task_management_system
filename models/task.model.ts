0178329cdf9cc2a9539e34c87146a8a4892f8b2c
21bowler <samwachira224@gmail.com>
2025-05-31 17:10:59 +0300

    add comment, activity and user assignment field

--- models/task.model.ts
+++ models/task.model.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document } from 'mongoose';
+import { IUser } from '@/models/user.model';
 
 // Interface for task document
 export interface ITask extends Document {
@@ -8,10 +9,85 @@ export interface ITask extends Document {
   priority: 'low' | 'medium' | 'high' | 'urgent';
   dueDate?: Date;
   tags?: string[];
+  assignedTo?: mongoose.Types.ObjectId | IUser;
+  sharedWith?: mongoose.Types.ObjectId | IUser[];
+  comments?: IComment[];
+  activityLog?: IActivityLog[];
+  createdBy: mongoose.Types.ObjectId | IUser;
   completedAt?: Date;
   createdAt: Date;
   updatedAt: Date;
 }
+
+// Comments Interface
+interface IComment extends Document {
+  content: string;
+  createdBy: mongoose.Types.ObjectId | IUser;
+  createdAt: Date;
+}
+
+//Activity Log
+interface IActivityLog extends Document {
+  action:
+    | 'created'
+    | 'updated'
+    | 'commented'
+    | 'assigned'
+    | 'shared'
+    | 'status-changed'
+    | 'priority-changed';
+  performedBy: mongoose.Types.ObjectId | IUser;
+  timestamp: Date;
+  details?: string;
+}
+
+// Comment Schema
+const CommentSchema = new Schema<IComment>(
+  {
+    content: {
+      type: String,
+      required: [true, 'Comment content is required.'],
+      trim: true,
+      maxlength: [500, 'Comment content cannot be more than 500 characters.'],
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'User who created the comment is required.'],
+    },
+  },
+  { timestamps: true },
+);
+
+// Activity log Schema
+const ActivityLogSchema = new Schema<IActivityLog>({
+  action: {
+    type: String,
+    enum: [
+      'created',
+      'updated',
+      'commented',
+      'assigned',
+      'shared',
+      'status-changed',
+      'priority-changed',
+    ],
+    required: [true, 'Action is required'],
+  },
+  performedBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'User who performed the action is required.'],
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+  details: {
+    type: String,
+    trim: true,
+  },
+});
 
 // Schema definition
 const TaskSchema = new Schema<ITask>(
@@ -46,8 +122,25 @@ const TaskSchema = new Schema<ITask>(
         trim: true,
       },
     ],
+    assignedTo: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    sharedWith: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    comments: [CommentSchema],
+    activityLog: [ActivityLogSchema],
     completedAt: {
       type: Date,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'User who created the task is required.'],
     },
   },
   {
